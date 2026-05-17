4791aa48a7bb4322b45c0806ed117547749fb388
Aaron Hersheson <Aarondeshaun59@gmail.com>
2026-05-17 16:25:27 +0100

    CRO & CSS

--- sticky-cta.js
+++ sticky-cta.js
@@ -1,16 +1,75 @@
 const stickyCTA = document.getElementById('stickyCTA');
+
 const stickyClose = document.getElementById('stickyClose');
+
+
+
+let ctaVisible = false;
+
+let hideTimer;
+
+
+
+function showCTA(){
+
+
+
+  if(ctaVisible) return;
+
+
+
+  ctaVisible = true;
+
+
+
+  stickyCTA.classList.add('show');
+
+
+
+  hideTimer = setTimeout(() => {
+
+    stickyCTA.classList.remove('show');
+
+    ctaVisible = false;
+
+  }, 5000);
+
+
+
+}
+
+
 
 window.addEventListener('scroll', () => {
 
-  if(window.scrollY > 700){
-    stickyCTA.classList.add('show');
-  } else {
-    stickyCTA.classList.remove('show');
+
+
+  if(window.scrollY > 700 && !ctaVisible){
+
+    showCTA();
+
   }
+
+
 
 });
 
+
+
 stickyClose.addEventListener('click', () => {
-  stickyCTA.style.display = 'none';
+
+
+
+  stickyCTA.classList.remove('show');
+
+
+
+  clearTimeout(hideTimer);
+
+
+
+  ctaVisible = false;
+
+
+
 });
